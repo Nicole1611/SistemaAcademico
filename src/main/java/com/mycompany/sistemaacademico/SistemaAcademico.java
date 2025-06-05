f36e9eb1ebfd3cb21a956d29e49583fa3fe12809
unknown <carolinavalladolidbravo@gmail.com>
2025-06-05 16:54:42 -0500

    Creacion de la clase profesor

--- src/main/java/com/mycompany/sistemaacademico/SistemaAcademico.java
+++ src/main/java/com/mycompany/sistemaacademico/SistemaAcademico.java
@@ -17,27 +17,27 @@ public class SistemaAcademico {
         System.out.println(""+persona.toString());
         //Persona invitada
         System.out.println("DATOS DEL OBJETO PERSONA INVITADA");
-        PersonalInvitado personaInvitada = new PersonalInvitado(2, "Jose", "r", "234");
+        PersonalInvitado personaInvitada = new PersonalInvitado(2, "Jose", "jose@gmail.com", "234");
         System.out.println(""+personaInvitada.toString());
         //Persona Institucional
         System.out.println("DATOS DEL OBJETO PERSONA INSTITUCIONAL");
-        PersonaInstitucional personaInstitucional = new PersonaInstitucional("Maria@es.edu.ec", 3, "Maria", "maria@gmail.com", "343851589-0");
+        PersonaInstitucional personaInstitucional = new PersonaInstitucional("maria@es.edu.ec", 3, "Maria", "maria@gmail.com", "343851589-0");
         System.out.println(""+personaInstitucional.toString());
         //Empleado
         System.out.println("DATOS DEL OBJETO EMPLEADO");
-        Empleado empleado = new Empleado(400, "Juan@esp.edu.ec", 4, "Juan", "Juan@gmail.com", "357863567-3");
+        Empleado empleado = new Empleado(400, "juan@esp.edu.ec", 4, "Juan", "juan@gmail.com", "357863567-3");
         System.out.println(""+empleado.toString());
         //Estudiante
         System.out.println("DATOS DEL OBJETO ESTUDIANTE");
-        Estudiantes estudiante = new Estudiantes(344, "Vicente@esp.ecu.ec", 5, "Vicente", "Vicente@gmail.com", "454548946-1");
+        Estudiantes estudiante = new Estudiantes(344, "vicente@esp.ecu.ec", 5, "Vicente", "vicente@gmail.com", "454548946-1");
         System.out.println(""+estudiante.toString());
         //Personal Academico
         System.out.println("DATOS DEL OBJETO PERSONAL ACADEMICO");
-        PersonalAcademico personalAcademico = new PersonalAcademico(644, "Darwin@esp.edu.ec", 6, "Darwin", "Darwin@gmail.com", "343589590-4");
+        PersonalAcademico personalAcademico = new PersonalAcademico(644, "darwin@esp.edu.ec", 6, "Darwin", "darwin@gmail.com", "343589590-4");
         System.out.println(""+personalAcademico.toString());
         //Personal Servicios
         System.out.println("DATOS DEL OBJETO PERSONAL ACADEMICO");
-        PersonalServicios personalServicios = new PersonalServicios("Tecnico", 200, "Blanca@esp.edu.ec", 7, "Blanca", "Blanca@gmail.com", "864524233-7");
+        PersonalServicios personalServicios = new PersonalServicios("Tecnico", 200, "blanca@esp.edu.ec", 7, "Blanca", "blanca@gmail.com", "864524233-7");
         System.out.println(""+personalServicios.toString());
     } 
 }
